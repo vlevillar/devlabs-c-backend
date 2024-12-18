9840aad8257974a65d25dd4be93c6d922e7d7576
Valentino Villar <vles2345@gmail.com>
2024-12-18 14:19:04 -0300

    connection to db

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("todo_database", "challenge_devlabs_user", "TbYqBpqmTKnes0Wbhf5zCjfXhmy0zE58", {
-  host: "dpg-cthfa79u0jms7384dbmg-a",
+const sequelize = new Sequelize("postgresql://challenge_devlabs_user:TbYqBpqmTKnes0Wbhf5zCjfXhmy0zE58@dpg-cthfa79u0jms7384dbmg-a.oregon-postgres.render.com/challenge_devlabs", {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl:{
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 export default sequelize;
